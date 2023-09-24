2c1130d47a3f4a3271a11da4b3bc7f3acfc18a27
Kaitlyn O’Flaherty <kaitlynoflaherty@Kaitlyns-MacBook-Pro.local>
2023-09-24 20:01:40 +0200

    make search exact match

--- pages/api/kits.js
+++ pages/api/kits.js
@@ -3,7 +3,7 @@ import kitsData from '../../data/kits.json';
 export default function handler(req, res) {
   const { query } = req.query;
   const filteredKits = kitsData.filter((kit) =>
-    kit.label_id.toLowerCase().includes(query.toLowerCase())
+    kit.label_id.toLowerCase() == (query.toLowerCase())
   );
   res.status(200).json(filteredKits);
 }
